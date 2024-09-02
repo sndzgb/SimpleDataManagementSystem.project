99c72a02d4033a641dbac8fea84982c9946f6e42
sandro <noemail@lackingdomain.co>
2024-09-02 07:05:19 +0200

    added action filter for modelState validation

---
+++ SimpleDataManagementSystem.Backend.WebAPI/Filters/ValidateModelActionFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using SimpleDataManagementSystem.Backend.WebAPI.WebApiModels.Read;
+
+namespace SimpleDataManagementSystem.Backend.WebAPI.Filters
+{
+    public class ValidateModelActionFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState.Values
+                                            .SelectMany(v => v.Errors)
+                                            .Select(e => e.ErrorMessage).ToList();
+
+                var model = new ErrorWebApiModel((int)HttpStatusCode.BadRequest, "One or more validation errors occured", errors);
+
+                var serializeOptions = new JsonSerializerOptions
+                {
+                    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    WriteIndented = true
+                };
+
+                context.Result = new ObjectResult(JsonSerializer.Serialize(model, serializeOptions))
+                {
+                    StatusCode = StatusCodes.Status400BadRequest
+                };
+            }
+        }
+    }
+}
