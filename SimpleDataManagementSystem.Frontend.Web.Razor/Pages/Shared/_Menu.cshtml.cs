f56b04919ed58a76cbd2dc3af5350316527efb5f
sandro <noemail@lackingdomain.co>
2024-09-02 07:40:58 +0200

    added default layout

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Shared/_Menu.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Shared
+{
+    public class _MenuModel : PageModel
+    {
+
+
+        public void OnGet()
+        {
+        }
+    }
+}
