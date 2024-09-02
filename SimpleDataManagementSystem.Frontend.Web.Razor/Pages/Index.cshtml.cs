db5b0e52909477b89e3674ece252d50b1b3db267
sandro <noemail@lackingdomain.co>
2024-09-02 07:39:54 +0200

    added index page

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [BindProperty]
+        public CredentialViewModel Credential { get; set; }
+
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
