e8ae6b7a5aa4e6a2127e7b7d56df99f536a7cd57
sandro <noemail@lackingdomain.co>
2024-09-02 07:44:40 +0200

    added exception for throwing errors during httpclient web calls

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Exceptions/WebApiCallException.cs
@@ -0,0 +1,32 @@
+﻿using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read;
+using System.Runtime.Serialization;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Exceptions
+{
+    public class WebApiCallException : Exception
+    {
+        public ErrorViewModel? Error { get; set; }
+
+
+        public WebApiCallException()
+        {
+        }
+        
+        public WebApiCallException(ErrorViewModel errorViewModel)
+        {
+            this.Error = errorViewModel;
+        }
+
+        public WebApiCallException(string? message) : base(message)
+        {
+        }
+
+        public WebApiCallException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected WebApiCallException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
