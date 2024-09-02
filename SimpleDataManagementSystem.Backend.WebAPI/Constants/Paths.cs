42de32794f363ba0e9fbbf874e2265032db262af
sandro <noemail@lackingdomain.co>
2024-09-02 07:04:02 +0200

    added new constant, web api base url, for viewing and uploading files

---
+++ SimpleDataManagementSystem.Backend.WebAPI/Constants/Paths.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleDataManagementSystem.Backend.WebAPI.Constants
+{
+    public static class Paths
+    {
+        public const string FILES_BASE_URL = "https://localhost:7006/api/files";
+    }
+}
