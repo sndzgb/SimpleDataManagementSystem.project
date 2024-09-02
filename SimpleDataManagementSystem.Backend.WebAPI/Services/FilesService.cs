a15bf9b4a2810c0b15e700579bde46b2b630f825
sandro <noemail@lackingdomain.co>
2024-09-02 07:23:59 +0200

    added file service, for storing uploaded user files, and deleting user uploaded files

---
+++ SimpleDataManagementSystem.Backend.WebAPI/Services/FilesService.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace SimpleDataManagementSystem.Backend.WebAPI.Services
+{
+    // TODO IFilesService; demo purposes only
+    public static class FilesService
+    {
+        public static void Upload(string relativePath, Stream stream)
+        {
+            var assDirPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+            using (var fs = new FileStream(Path.Combine(assDirPath, relativePath), FileMode.CreateNew))
+            {
+                stream.CopyToAsync(fs).GetAwaiter().GetResult();
+            }
+        }
+
+        public static void Delete(string relativePath)
+        {
+            var assDirPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+            File.Delete(Path.Combine(assDirPath, relativePath)); // TODO error handling; + log error & continue
+        }
+    }
+}
