02de74aa6f7026d2602b889b6f1b84439945b4a2
sandro <noemail@lackingdomain.co>
2024-09-02 07:47:31 +0200

    added new data annotation validator, used to validate decimal values

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Validators/DecimalValidator.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Validators
+{
+    // TODO move to shared project
+    public class DecimalValidator : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            decimal nr;
+
+            bool isDecimal = decimal.TryParse(value.ToString(), CultureInfo.InvariantCulture, out nr);
+
+            if (!isDecimal)
+            {
+                ErrorMessage = "Invalid decimal value";
+            }
+
+            return isDecimal;
+        }
+    }
+}
