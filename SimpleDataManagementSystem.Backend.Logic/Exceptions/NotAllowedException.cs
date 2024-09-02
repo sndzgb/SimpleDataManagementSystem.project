171a46565d5eb65d9516f134e0fce3326d4f4ac1
sandro <noemail@lackingdomain.co>
2024-09-02 06:50:45 +0200

    added new exception

---
+++ SimpleDataManagementSystem.Backend.Logic/Exceptions/NotAllowedException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataManagementSystem.Backend.Logic.Exceptions
+{
+    public class NotAllowedException : Exception
+    {
+        public NotAllowedException()
+        {
+        }
+
+        public NotAllowedException(string? message) : base(message)
+        {
+        }
+
+        public NotAllowedException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotAllowedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
