08caea1c374a18d908ef46e2a33db22ec44bc879
sandro <noemail@lackingdomain.co>
2024-09-05 07:25:47 +0200

    added new options, JwtOptions

---
+++ SimpleDataManagementSystem.Backend.WebAPI/Options/JwtOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleDataManagementSystem.Backend.WebAPI.Options
+{
+    public class JwtOptions
+    {
+        /// <summary>
+        /// Section name.
+        /// </summary>
+        public const string Jwt = "JwtOptions";
+
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Key { get; set; }
+        public int ExpiresInMinutes { get; set; }
+    }
+}
