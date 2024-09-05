4bffc75dcb2d7590924c420c24e170aa3b871856
sandro <noemail@lackingdomain.co>
2024-09-05 07:22:53 +0200

    added new helper, string to decimal converter

---
+++ SimpleDataManagementSystem.Backend.WebAPI/Helpers/DecimalHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace SimpleDataManagementSystem.Backend.WebAPI.Helpers
+{
+    public static class DecimalHelpers
+    {
+        public static decimal GetDecimalFromString(string @decimal)
+        {
+            var decimalAsString = @decimal.Replace(",", ".");
+
+            Char replaceChar = '.';
+            
+            int lastIndex = decimalAsString.LastIndexOf(replaceChar);
+            
+            if (lastIndex != -1)
+            {
+                IEnumerable<Char> chars = decimalAsString.Where((c, i) => c != replaceChar || i == lastIndex);
+                decimalAsString = new string(chars.ToArray());
+            }
+
+            decimal d = decimal.Parse(decimalAsString, new NumberFormatInfo() { NumberDecimalSeparator = "." });
+
+            return d;
+        }
+    }
+}
