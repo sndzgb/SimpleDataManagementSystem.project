562786bbb303594578c31f9653494ed024fcaee1
sandro <noemail@lackingdomain.co>
2024-09-05 09:02:50 +0200

    added new component - Pager

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/ViewComponents/PagerViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.ViewComponents
+{
+    public class PagerViewComponent : ViewComponent
+    {
+        public Paged Paged { get; set; }
+
+
+        public IViewComponentResult Invoke(PagedViewModel pageInfo)
+        {
+            Paged = new Paged(pageInfo);
+            return View(Paged);
+        }
+    }
+
+    public class Paged
+    {
+        public Paged(PagedViewModel pagedViewModel)
+        {
+            this.PageInfo = pagedViewModel;
+        }
+
+        public PagedViewModel PageInfo { get; private set; }
+    }
+}
