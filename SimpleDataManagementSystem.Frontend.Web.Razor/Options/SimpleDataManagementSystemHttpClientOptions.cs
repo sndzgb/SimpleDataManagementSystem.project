9aed8db099747cf4fd541cb917599cdbaad6f319
sandro <noemail@lackingdomain.co>
2024-09-05 08:23:00 +0200

    added httpClient options

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Options/SimpleDataManagementSystemHttpClientOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleDataManagementSystem.Frontend.Web.Razor.Options
+{
+    public class SimpleDataManagementSystemHttpClientOptions
+    {
+        /// <summary>
+        /// Section name.
+        /// </summary>
+        public const string SimpleDataManagementSystemHttpClient = "SimpleDataManagementSystemHttpClientOptions";
+
+        public string Url { get; set; }
+    }
+}
