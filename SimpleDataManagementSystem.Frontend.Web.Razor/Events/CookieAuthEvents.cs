4b6d642a8331f04798079a0c820380644bc73b0d
sandro <noemail@lackingdomain.co>
2024-09-05 08:51:33 +0200

    added new CookieAuthEvents override

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Events/CookieAuthEvents.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Events
+{
+    public class CookieAuthEvents : CookieAuthenticationEvents
+    {
+        public override Task RedirectToAccessDenied(RedirectContext<CookieAuthenticationOptions> context)
+        {
+            context.RedirectUri = "/Account/AccessDenied";
+
+            return base.RedirectToAccessDenied(context);
+        }
+    }
+}
