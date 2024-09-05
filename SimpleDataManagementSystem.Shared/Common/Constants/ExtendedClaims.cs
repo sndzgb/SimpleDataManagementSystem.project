d22d87abb92af7261354f32acf55f77c4d6598e9
sandro <noemail@lackingdomain.co>
2024-09-05 09:11:14 +0200

    added shared projects constants

---
+++ SimpleDataManagementSystem.Shared/Common/Constants/ExtendedClaims.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataManagementSystem.Shared.Common.Constants
+{
+    public static class ExtendedClaims
+    {
+        public class Type
+        {
+            public const string UserId = "UserId";
+            public const string Role = "Role";
+            public const string Username = "Username";
+            public const string Jwt = "Jwt";
+        }
+    }
+}
