73ccc604a41bf8a0fa23a869eddcdafc103348f9
sandro <noemail@lackingdomain.co>
2024-09-05 09:11:30 +0200

    added shared projects validators

---
+++ SimpleDataManagementSystem.Shared/Web/Validators/DecimalValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataManagementSystem.Shared.Web.Validators
+{
+    public class DecimalValidator : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            decimal nr;
+
+            bool isDecimal = decimal.TryParse(value.ToString(), CultureInfo.InvariantCulture, out nr);
+
+            if (!isDecimal)
+            {
+                ErrorMessage = "Invalid decimal value";
+            }
+
+            return isDecimal;
+        }
+    }
+}
---
+++ SimpleDataManagementSystem.Shared/Web/Validators/MaxFileSizeValidator.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataManagementSystem.Shared.Web.Validators
+{
+    /// <summary>
+    /// Validates uploaded files' size in bytes.
+    /// </summary>
+    public class MaxFileSizeValidator : ValidationAttribute
+    {
+        private readonly int _maxFileSizeBytes;
+
+
+        public MaxFileSizeValidator(int maxFileSizeBytes)
+        {
+            _maxFileSizeBytes = maxFileSizeBytes;
+        }
+
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+
+            if (file != null)
+            {
+                if (file.Length > _maxFileSizeBytes)
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        public string GetErrorMessage()
+        {
+            return $"Maximum allowed file size is {_maxFileSizeBytes} bytes.";
+        }
+    }
+}
