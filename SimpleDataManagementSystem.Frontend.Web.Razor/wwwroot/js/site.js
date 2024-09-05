afcfe7d0a00cc87918a8382e95aef1d16f803af3
sandro <noemail@lackingdomain.co>
2024-09-05 08:53:44 +0200

    added javascript snackbar invoke method

--- SimpleDataManagementSystem.Frontend.Web.Razor/wwwroot/js/site.js
+++ SimpleDataManagementSystem.Frontend.Web.Razor/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function showSnackbar(message) {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar");
+
+    x.innerHTML = message;
+
+    // Add the "show" class to DIV
+    x.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function () {
+        x.className = x.className.replace("show", "");
+
+        // Replace with default placeholder
+        var element = document.createElement("em");
+        element.appendChild(document.createTextNode('Placeholder Test'));
+        document.getElementById('snackbar').appendChild(element);
+    }, 3000);
+}
