79ebddd4d5cc9201108904b50efc8dbb9581d27d
sandro <noemail@lackingdomain.co>
2024-09-05 07:00:07 +0200

    added new exceptions

---
+++ SimpleDataManagementSystem.Backend.Logic/Exceptions/RecordExistsException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataManagementSystem.Backend.Logic.Exceptions
+{
+    public class RecordExistsException : Exception
+    {
+        public RecordExistsException()
+        {
+        }
+
+        public RecordExistsException(string? message) : base(message)
+        {
+        }
+
+        public RecordExistsException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected RecordExistsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ SimpleDataManagementSystem.Backend.Logic/Exceptions/RequiredRecordNotFoundException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataManagementSystem.Backend.Logic.Exceptions
+{
+    public class RequiredRecordNotFoundException : Exception
+    {
+        public RequiredRecordNotFoundException()
+        {
+        }
+
+        public RequiredRecordNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public RequiredRecordNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected RequiredRecordNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
