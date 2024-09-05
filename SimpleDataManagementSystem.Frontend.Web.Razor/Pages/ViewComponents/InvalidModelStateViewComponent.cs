d0dd5a5121153b66d3fa79af05624f92da61be5f
sandro <noemail@lackingdomain.co>
2024-09-05 09:02:31 +0200

    added new component - InvalidModelState

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/ViewComponents/InvalidModelStateViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.ViewComponents
+{
+    public class InvalidModelStateViewComponent : ViewComponent
+    {
+        public ModelStateDictionary? ModelStateDictionary { get; set; }
+
+
+        public IViewComponentResult Invoke(ModelStateDictionary? modelStateDictionary)
+        {
+            ModelStateDictionary = modelStateDictionary;
+
+            return View(model: ModelStateDictionary);
+        }
+    }
+}
