1494c007a28b58f28c9dd6ba4485286a0de6e369
sandro <noemail@lackingdomain.co>
2024-10-23 09:24:53 +0200

    updated user record with new required field

--- SimpleDataManagementSystem.Backend.WebAPI/WebApiModels/Records/AuthenticatedUser.cs
+++ SimpleDataManagementSystem.Backend.WebAPI/WebApiModels/Records/AuthenticatedUser.cs
@@ -1,4 +1,4 @@
 ﻿namespace SimpleDataManagementSystem.Backend.WebAPI.WebApiModels.Records
 {
-    public record AuthenticatedUser(int UserId, string Username, string[] Roles);
+    public record AuthenticatedUser(int UserId, string Username, string[] Roles, bool IsPasswordChangeRequired);
 }
