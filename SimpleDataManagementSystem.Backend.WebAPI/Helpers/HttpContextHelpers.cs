7ccadadd67310449b811847afdda0ede8e574d3f
sandro <noemail@lackingdomain.co>
2024-10-23 09:20:54 +0200

    added helper methods for working with policies

---
+++ SimpleDataManagementSystem.Backend.WebAPI/Helpers/HttpContextHelpers.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using SimpleDataManagementSystem.Shared.Common.Constants;
+
+namespace SimpleDataManagementSystem.Backend.WebAPI.Helpers
+{
+    public static class HttpContextHelpers
+    {
+        public static int? GetAuthenticatedUserIdFromHttpContext(HttpContext httpContext)
+        {
+            if (httpContext == null)
+            {
+                return null;
+            }
+
+            if (
+                int.TryParse
+                (
+                    httpContext
+                    !.User
+                    !.Claims
+                    ?.Where(x => x.Type == ExtendedClaims.Type.UserId)
+                    .FirstOrDefault()
+                    ?.Value, out int userId
+                )
+            )
+            {
+                return userId;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static string GetClaimValueByType(string claimType, HttpContext httpContext)
+        {
+            return httpContext
+                !.User
+                !.Claims
+                ?.Where(x => x.Type == claimType)
+                .FirstOrDefault()
+                ?.Value ?? "";
+        }
+    }
+}
