1c7eb3db8c634e4fa8c62b60b4913cf97fc977ae
sandro <noemail@lackingdomain.co>
2024-10-23 09:42:14 +0200

    added new component for displaying errors to client

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/ViewComponents/DisplayErrorViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.ViewComponents
+{
+    public class DisplayErrorViewComponent : ViewComponent
+    {
+        public ErrorViewModel Error { get; set; }
+
+
+        public IViewComponentResult Invoke(ErrorViewModel errorViewModel)
+        {
+            Error = errorViewModel;
+            return View(model: Error);
+        }
+    }
+}
