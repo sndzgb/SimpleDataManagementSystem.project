7a411463b65de978bb850b0cb4693e90efe98885
sandro <noemail@lackingdomain.co>
2024-10-23 09:29:12 +0200

    added new policies

---
+++ SimpleDataManagementSystem.Shared/Common/Constants/Policies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataManagementSystem.Shared.Common.Constants
+{
+    public class Policies
+    {
+        public PolicyNames Names; // { get; set; }
+        
+        public class PolicyNames
+        {
+            public const string UserIsResourceOwner = "UserIsResourceOwnerPolicy";
+            public const string UserIsInRole = "UserIsInRolePolicy";
+        }
+    }
+}
