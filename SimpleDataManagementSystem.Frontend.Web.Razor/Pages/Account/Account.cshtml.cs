fff172e1575464f25173a69bec13a243a7b18922
sandro <noemail@lackingdomain.co>
2024-10-23 09:47:59 +0200

    updated login page

--- SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Account/Account.cshtml.cs
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Account/Account.cshtml.cs
@@ -2,9 +2,12 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Constants;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Exceptions;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Helpers;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Services;
+using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read;
 using SimpleDataManagementSystem.Shared.Common.Constants;
+using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
@@ -22,44 +25,80 @@ namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Account
         }
 
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
+            return null;
+            //return RedirectToPage("/Index");
         }
+
+        
+        public ErrorViewModel Error { get; set; }
+
+        [BindProperty]
+        public CredentialViewModel Credential { get; set; }
+
 
         public async Task<IActionResult> OnPostLogout()
         {
             await HttpContext.SignOutAsync();
 
-            return RedirectToPage("/Index");
+            return RedirectToPage("/Account/Account"); // login
+            //return RedirectToPage("/Index");
         }
 
-        public async Task<IActionResult> OnPostLogin(string username, string password)
+        //async Task<IActionResult>
+        public async Task<IActionResult> OnPost(string username, string password) //OnPostLogin
         {
-            var token = await _accountsService.LogInAsync(username, password);
+            //try
+            //{
+                var token = await _accountsService.LogInAsync(username, password);
 
-            if (token == null)
-            {
-                return RedirectToPage("/Index");
-            }
+                if (token == null)
+                {
+                    Error = new ErrorViewModel(401, "Login failed.", null);
 
-            var claims = ClaimsHelper.GetClaimsFromToken(token);
+                    //ViewData["Error"] = "Login failed.";
+                    //return;
+                    //return Page();
+                    return await OnGet();
+                    //return RedirectToPage("/Index");
+                }
 
-            var identity = new ClaimsIdentity(claims, Cookies.AuthenticationCookie.Name);
+                var claims = ClaimsHelper.GetClaimsFromToken(token);
 
-            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+                var identity = new ClaimsIdentity(claims, Cookies.AuthenticationCookie.Name);
 
-            await HttpContext.SignInAsync(Cookies.AuthenticationCookie.Name, claimsPrincipal);
-            
-            string returnUrl = HttpContext.Request.Query["returnUrl"];
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
 
-            if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
-            {
-                return Redirect(returnUrl);
-            }
-            else
-            {
-                return RedirectToPage("/Index");
-            }
+                if (Convert.ToBoolean(claims.Where(x => x.Type == ExtendedClaims.Type.IsPasswordChangeRequired).FirstOrDefault().Value))
+                {
+                    var userId = claims.Where(x => x.Type == ClaimTypes.NameIdentifier).FirstOrDefault().Value;
+                    ViewData["UserId"] = userId;
+
+                    return LocalRedirect($"/Account/{userId}/ChangePassword");
+
+                    //return RedirectToPage("/Account/{UserId}/ChangePassword", new { UserId = userId });
+                    //return new RedirectToPageResult("/Account/ChangePassword");
+                    //return RedirectToPage("/Account/ChangePassword");
+                }
+
+                await HttpContext.SignInAsync(Cookies.AuthenticationCookie.Name, claimsPrincipal);
+
+                string returnUrl = HttpContext.Request.Query["returnUrl"];
+
+                if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToPage("/Index");
+                }
+            //}
+            //catch (HttpRequestException e) when (e.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            //{
+
+            //}
         }
     }
 }
