5c26772eb22475de9dbe03b955dc37395610b3fb
sandro <noemail@lackingdomain.co>
2024-10-23 09:26:03 +0200

    added new model, for password update

---
+++ SimpleDataManagementSystem.Backend.WebAPI/WebApiModels/Write/UpdatePasswordWebApiModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleDataManagementSystem.Backend.WebAPI.WebApiModels.Write
+{
+    public class UpdatePasswordWebApiModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Old password must not be empty")]
+        public string OldPassword { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "New password must not be empty")]
+        public string NewPassword { get; set; }
+    }
+}
