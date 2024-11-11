f3a3b7b105c28d9345c29632069a0c6181da184f
sandro <noemail@lackingdomain.co>
2024-11-11 14:47:17 +0100

    added new helper - permissions helper which is used to validate whether a user is permitted to do/ view an action

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Helpers/PermissionsHelper.cs
@@ -0,0 +1,27 @@
+﻿using SimpleDataManagementSystem.Shared.Common.Constants;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Helpers
+{
+    public static class PermissionsHelper
+    {
+        public static bool IsInRole(Roles requiredRole, string userRoleName)
+        {
+            if (!Enum.TryParse(userRoleName, out Roles role))
+            {
+                return false;
+            }
+
+            if (requiredRole != role)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsResourceOwner(string resourceOwnerId, string userId)
+        {
+            return false;
+        }
+    }
+}
