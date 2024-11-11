be46219b5ac9f4cc5ddfb7290e8820b0007571f0
sandro <noemail@lackingdomain.co>
2024-11-11 15:21:55 +0100

    added new model to store authentication token when user successfully logs in

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/ViewModels/Read/AuthTokenViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read
+{
+    public class AuthTokenViewModel
+    {
+        [JsonPropertyName("jwt")]
+        public string? Jwt { get; set; }
+    }
+}
