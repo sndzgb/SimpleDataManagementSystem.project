a70c94c487575cbff3f4c8dfc00f7795b35de57e
sandro <noemail@lackingdomain.co>
2024-11-11 15:07:41 +0100

    added navbar active link styling for html helper (new extension)

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string ActiveClass(this IHtmlHelper htmlHelper, string routeStartsWith)
+        {
+            var routeData = htmlHelper.ViewContext.RouteData;
+
+            var pageRoute = routeData.Values["page"]?.ToString();
+
+            return pageRoute!.StartsWith(routeStartsWith) ? "active-navbar-link" : "";
+        }
+    }
+}
