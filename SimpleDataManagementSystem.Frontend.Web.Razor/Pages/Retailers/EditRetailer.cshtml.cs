af73476b7a928d542f2e1f7885f79f2835baa697
sandro <noemail@lackingdomain.co>
2024-11-11 14:26:24 +0100

    updated edit retailer - it now inherits from base page model; implemented authorization based on defined policies; updated html

--- SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Retailers/EditRetailer.cshtml.cs
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Retailers/EditRetailer.cshtml.cs
@@ -1,25 +1,28 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Exceptions;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Base;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Services;
 using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read;
 using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Write;
+using SimpleDataManagementSystem.Shared.Common.Constants;
 using System.Net;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Retailers
 {
-    [Authorize(Roles = "Admin,Employee")]
     [ValidateAntiForgeryToken]
-    public class EditRetailerModel : PageModel
+    public class EditRetailerModel : BasePageModel<UpdateRetailerViewModel>
     {
         private readonly IRetailersService _retailersService;
+        private readonly IAuthorizationService _authorizationService;
 
-
-        public EditRetailerModel(IRetailersService retailersService)
+        public EditRetailerModel(IRetailersService retailersService, IAuthorizationService authorizationService)
         {
             _retailersService = retailersService;
+            _authorizationService = authorizationService;
         }
 
 
@@ -28,37 +31,39 @@ namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Retailers
 
         public RetailerViewModel Retailer { get; set; }
 
-        [BindProperty]
-        public UpdateRetailerViewModel UpdatedRetailer { get; set; }
 
-        public WebApiCallException Error { get; set; }
+        public override async void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+        {
+            int[] roles = new int[] { (int)Roles.Admin, (int)Roles.Employee };
+
+            AuthorizationResult authorizationResult = await _authorizationService.AuthorizeAsync(
+                HttpContext.User,
+                new { roles },
+                Policies.PolicyNames.UserIsInRole
+            );
+
+            if (!authorizationResult.Succeeded)
+            {
+                context.Result = Forbid();
+                return;
+            }
+
+            base.OnPageHandlerExecuting(context);
+        }
 
 
         public async Task<IActionResult> OnGet()
         {
             Retailer = await _retailersService.GetRetailerByIdAsync(RetailerId);
 
-            return null;
+            return Page();
         }
 
         public async Task<IActionResult> OnPost()
         {
-            try
-            {
-                if (!ModelState.IsValid)
-                {
-                    return await OnGet();
-                }
+            await _retailersService.UpdateRetailerAsync(RetailerId, Model);
 
-                await _retailersService.UpdateRetailerAsync(RetailerId, UpdatedRetailer);
-
-                return RedirectToPage("/Retailers/Retailers");
-            }
-            catch (WebApiCallException wace)
-            {
-                Error = wace; // set error on model
-                return await OnGet();
-            }
+            return RedirectToPage("/Retailers/Retailers");
         }
 
         public async Task<IActionResult> OnPostDeleteImage([FromRoute] int retailerId)
