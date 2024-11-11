15ac1123e0bba490a2d015cd11472fdabe5d94cc
sandro <noemail@lackingdomain.co>
2024-11-11 13:44:40 +0100

    added new helper for enums, checks whether the specified enum value is defined or not

---
+++ SimpleDataManagementSystem.Shared/Helpers/EnumHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataManagementSystem.Shared.Helpers
+{
+    public static class EnumHelpers
+    {
+        public static bool IsDefined<T>(int value)
+        {
+            var values = Enum.GetValues(typeof(T)).Cast<int>().OrderBy(x => x);
+
+            return values.Contains(value);
+        }
+    }
+}
