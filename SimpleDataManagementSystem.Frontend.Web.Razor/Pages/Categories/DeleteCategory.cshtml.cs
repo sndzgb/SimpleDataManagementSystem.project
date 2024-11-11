265f1d029d15019af259d16ba0860052bda0025d
sandro <noemail@lackingdomain.co>
2024-11-11 14:34:52 +0100

    updated delete category - it now inherits from base page model; implemented authorization based on defined policies; updated html to reflect those changes

--- SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Categories/DeleteCategory.cshtml.cs
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Categories/DeleteCategory.cshtml.cs
@@ -1,21 +1,26 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Base;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Services;
+using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read;
+using SimpleDataManagementSystem.Shared.Common.Constants;
 using System.Net;
 
 namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Categories
 {
     [ValidateAntiForgeryToken]
-    [Authorize(Roles = "Admin,Employee")]
-    public class DeleteCategoryModel : PageModel
+    public class DeleteCategoryModel : BasePageModel<CategoryViewModel>
     {
         private readonly ICategoriesService _categoriesService;
+        private readonly IAuthorizationService _authorizationService;
 
 
-        public DeleteCategoryModel(ICategoriesService categoriesService)
+        public DeleteCategoryModel(ICategoriesService categoriesService, IAuthorizationService authorizationService)
         {
             _categoriesService = categoriesService;
+            _authorizationService = authorizationService;
         }
 
 
@@ -23,15 +28,38 @@ namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Categories
         public int CategoryId { get; set; }
 
 
-        public void OnGet()
+        public override async void OnPageHandlerExecuting(PageHandlerExecutingContext context)
         {
+            int[] roles = new int[] { (int)Roles.Admin, (int)Roles.Employee };
+
+            AuthorizationResult authorizationResult = await _authorizationService.AuthorizeAsync(
+                HttpContext.User,
+                new { roles },
+                Policies.PolicyNames.UserIsInRole
+            );
+
+            if (!authorizationResult.Succeeded)
+            {
+                context.Result = Forbid();
+                return;
+            }
+
+            base.OnPageHandlerExecuting(context);
         }
 
-        public async Task<IActionResult> OnPostDeleteCategory()
+
+        public async Task<IActionResult> OnGet()
+        {
+            Model = await _categoriesService.GetCategoryByIdAsync(CategoryId);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
         {
             await _categoriesService.DeleteCategoryAsync(CategoryId);
 
-            return new JsonResult(null) { StatusCode = (int)HttpStatusCode.OK };
+            return RedirectToPage("/Categories/Categories");
         }
     }
 }
