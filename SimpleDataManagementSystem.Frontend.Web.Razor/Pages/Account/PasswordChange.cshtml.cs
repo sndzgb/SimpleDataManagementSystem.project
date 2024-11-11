6091f7ed3fa9da8dde7089cd720afbc3096387f9
sandro <noemail@lackingdomain.co>
2024-11-11 14:40:43 +0100

    added password change page

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Account/PasswordChange.cshtml.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Exceptions;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Base;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Services;
+using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Write;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Account
+{
+    [ValidateAntiForgeryToken]
+    [AllowAnonymous]
+    public class PasswordChangeModel : BasePageModel<UpdatePasswordViewModel>
+    {
+        private readonly IAccountsService _accountsService;
+        private readonly IUsersService _usersService;
+
+
+        public PasswordChangeModel(IAccountsService accountsService, IUsersService usersService)
+        {
+            _accountsService = accountsService;
+            _usersService = usersService;
+        }
+
+
+        public async Task<IActionResult> OnGet()
+        {
+            if (!(User.Identity!.IsAuthenticated))
+            {
+                return RedirectToPage("/Account/Login");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _accountsService.UpdatePasswordAsync(Model?.OldPassword, Model?.NewPassword);
+
+            await HttpContext.SignOutAsync();
+
+            return RedirectToPage("");
+        }
+    }
+}
