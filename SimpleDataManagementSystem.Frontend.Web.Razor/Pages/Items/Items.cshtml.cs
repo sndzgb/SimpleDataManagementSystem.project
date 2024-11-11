69e116892bee3ed19fb34e71d3856a75e0440507
sandro <noemail@lackingdomain.co>
2024-11-11 14:32:24 +0100

    updated items - it now inherits from base page model; implemented authorization based on defined policies; updated html to reflect those changes

--- SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Items/Items.cshtml.cs
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Items/Items.cshtml.cs
@@ -1,28 +1,52 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Exceptions;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Base;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Services;
 using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read;
 using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Write;
+using SimpleDataManagementSystem.Shared.Common.Constants;
 
 namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Items
 {
-    public class ItemsModel : PageModel
+    public class ItemsModel : BasePageModel<ItemsViewModel>
     {
         private readonly IItemsService _itemsService;
+        private readonly IAuthorizationService _authorizationService;
 
 
-        public ItemsModel(IItemsService itemsService)
+        public ItemsModel(IItemsService itemsService, IAuthorizationService authorizationService)
         {
             _itemsService = itemsService;
+            _authorizationService = authorizationService;
         }
 
 
-        public ItemsViewModel Items { get; set; }
+        public override async void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+        {
+            int[] roles = new int[] { (int)Roles.Admin, (int)Roles.Employee };
+
+            AuthorizationResult authorizationResult = await _authorizationService.AuthorizeAsync(
+                HttpContext.User,
+                new { roles },
+                Policies.PolicyNames.UserIsInRole
+            );
+
+            if (!authorizationResult.Succeeded)
+            {
+                context.Result = Forbid();
+                return;
+            }
+
+            base.OnPageHandlerExecuting(context);
+        }
 
 
         public async Task OnGet([FromQuery] int take = 8, [FromQuery] int page = 1)
         {
-            Items = await _itemsService.GetAllItemsAsync(take, page);
+            Model = await _itemsService.GetAllItemsAsync(take, page);
 
             return;
         }
