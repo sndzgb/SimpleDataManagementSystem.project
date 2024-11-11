ffd1ab3373faba053aee81bdffd58d8938de18ac
sandro <noemail@lackingdomain.co>
2024-11-11 14:19:25 +0100

    updated create user - it now inherits from base page model; updated html

--- SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Users/CreateUser.cshtml.cs
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Users/CreateUser.cshtml.cs
@@ -1,65 +1,67 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using SimpleDataManagementSystem.Frontend.Web.Razor.Constants;
-using SimpleDataManagementSystem.Frontend.Web.Razor.Exceptions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Base;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Services;
 using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read;
 using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Write;
 using SimpleDataManagementSystem.Shared.Common.Constants;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages
 {
-    [Authorize(Roles = "Admin")]
-    public class CreateUserModel : PageModel
+    public class CreateUserModel : BasePageModel<NewUserViewModel>
     {
         private readonly IUsersService _usersService;
         private readonly IRolesService _rolesService;
+        private readonly IAuthorizationService _authorizationService;
 
 
-        public CreateUserModel(IUsersService usersService, IRolesService rolesService)
+        public CreateUserModel(IUsersService usersService, IRolesService rolesService, IAuthorizationService authorizationService)
         {
             _usersService = usersService;
             _rolesService = rolesService;
+            _authorizationService = authorizationService;
         }
 
 
-        [BindProperty]
-        public NewUserViewModel NewUser { get; set; }
-
         public List<RoleViewModel> AvailableRoles { get; set; }
 
-        public WebApiCallException Error { get; set; }
+
+        public override async void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+        {
+            int[] roles = new int[] { (int)Roles.Admin };
+
+            AuthorizationResult authorizationResult = await _authorizationService.AuthorizeAsync(
+                HttpContext.User,
+                new { roles },
+                Policies.PolicyNames.UserIsInRole
+            );
+
+            if (!authorizationResult.Succeeded)
+            {
+                context.Result = Forbid();
+                return;
+            }
+
+            base.OnPageHandlerExecuting(context);
+        }
 
 
         public async Task<IActionResult> OnGet()
         {
             var roles = await _rolesService.GetAllRolesAsync();
 
+            // TODO filter roles on server!
             AvailableRoles = roles.Where(x => x.Id != (int)Roles.Admin).ToList(); // creating new admins not allowed
 
-            return null;
+            return Page();
         }
 
         public async Task<IActionResult> OnPost(NewUserViewModel newUserViewModel)
         {
-            try
-            {
-                if (!ModelState.IsValid)
-                {
-                    return await OnGet();
-                }
+            var newUserId = await _usersService.AddNewUserAsync(newUserViewModel);
 
-                var newUserId = await _usersService.AddNewUserAsync(newUserViewModel);
-
-                return RedirectToPage("/Users/Users");
-            }
-            catch (WebApiCallException wace)
-            {
-                Error = wace; // set error on model
-                return await OnGet();
-            }
+            return RedirectToPage("/Users/Users");
         }
     }
 }
