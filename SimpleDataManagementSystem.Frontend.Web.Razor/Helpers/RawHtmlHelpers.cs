86cc30626a80c234d96b3e61248de4fe90af1c40
sandro <noemail@lackingdomain.co>
2024-11-12 00:14:37 +0100

    added new helper function, sanitizes provided raw html into safe html string

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Helpers/RawHtmlHelpers.cs
@@ -0,0 +1,13 @@
+﻿using Ganss.Xss;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Helpers
+{
+    public static class RawHtmlHelpers
+    {
+        public static string SanitizeHtmlString(string html)
+        {
+            var sanitizer = new HtmlSanitizer();
+            return sanitizer.Sanitize(html);
+        }
+    }
+}
