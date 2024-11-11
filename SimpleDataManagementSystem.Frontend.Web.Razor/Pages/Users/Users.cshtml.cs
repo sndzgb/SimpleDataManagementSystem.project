1071d4f91c8ea6bf8915b9f8ab085c05c89d861b
sandro <noemail@lackingdomain.co>
2024-11-11 14:23:19 +0100

    updated users - it now inherits from base page model; implemented authorization based on defined policies; updated html

--- SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Users/Users.cshtml.cs
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Users/Users.cshtml.cs
@@ -1,22 +1,26 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Exceptions;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Base;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Services;
 using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read;
 using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Write;
+using SimpleDataManagementSystem.Shared.Common.Constants;
 
 namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages
 {
-    [Authorize(Policy = "Admin")]
-    public class UsersModel : PageModel
+    public class UsersModel : BasePageModel<UsersViewModel>
     {
         private readonly IUsersService _usersService;
+        private readonly IAuthorizationService _authorizationService;
 
 
-        public UsersModel(IUsersService usersService)
+        public UsersModel(IUsersService usersService, IAuthorizationService authorizationService)
         {
             _usersService = usersService;
+            _authorizationService = authorizationService;
         }
 
 
@@ -29,16 +33,30 @@ namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages
         //[BindProperty(Name = "ipp", SupportsGet = true)]
         //public int ItemsPerPage { get; set; } = 8;
 
-
-        public async Task OnGet([FromQuery] int take = 8, [FromQuery] int page = 1)
+        public override async void OnPageHandlerExecuting(PageHandlerExecutingContext context)
         {
-            Users = await _usersService.GetAllUsersAsync(take, page);
-            
-            return;
+            int[] roles = new int[] { (int)Roles.Admin };
+
+            AuthorizationResult authorizationResult = await _authorizationService.AuthorizeAsync(
+                HttpContext.User,
+                new { roles },
+                Policies.PolicyNames.UserIsInRole
+            );
+
+            if (!authorizationResult.Succeeded)
+            {
+                context.Result = Forbid();
+                return;
+            }
+
+            base.OnPageHandlerExecuting(context);
         }
 
-        public void OnPost()
+
+        public async Task<IActionResult> OnGet([FromQuery] int take = 8, [FromQuery] int page = 1)
         {
+            Users = await _usersService.GetAllUsersAsync(take, page);
+            return Page();
         }
     }
 }
