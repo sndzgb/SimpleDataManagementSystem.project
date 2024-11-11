c3845d7954cf6c7ce3060aaedab8563c9e684e6f
sandro <noemail@lackingdomain.co>
2024-11-11 15:25:25 +0100

    updated create new item view model with new required field validators

--- SimpleDataManagementSystem.Frontend.Web.Razor/ViewModels/Write/NewItemViewModel.cs
+++ SimpleDataManagementSystem.Frontend.Web.Razor/ViewModels/Write/NewItemViewModel.cs
@@ -10,22 +10,25 @@ namespace SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Write
         [StringLength(maximumLength: 255, ErrorMessage = "Invalid item name", MinimumLength = 2)]
         public string Nazivproizvoda { get; set; }
 
-        [Required(ErrorMessage = "Opis is required")]
-        [StringLength(maximumLength: 255, ErrorMessage = "Invalid opsi", MinimumLength = 2)]
-        public string Opis { get; set; }
+        //[Required(ErrorMessage = "Opis is required")]
+        [StringLength(maximumLength: 255, ErrorMessage = "Opis is invalid")]
+        public string? Opis { get; set; }
 
         //[Required(ErrorMessage = "Datum akcije is required")]
-        //[StringLength(maximumLength: 255, ErrorMessage = "Invalid datum akcije", MinimumLength = 2)]
+        [StringLength(maximumLength: 255, ErrorMessage = "Invalid datum akcije", MinimumLength = 2)]
         public string? Datumakcije { get; set; }
 
         [Required(ErrorMessage = "Retailer is required")]
-        public int RetailerId { get; set; }
+        public int? RetailerId { get; set; }
 
+        [Required]
         [DecimalValidator]
         public string Cijena { get; set; }
 
         [Required(ErrorMessage = "Kategorija is required")]
-        public int Kategorija { get; set; }
+        [MaxLength(255, ErrorMessage = "Kategorija invalid max value")]
+        [MinLength(0, ErrorMessage = "Kategorija invalid min value")]
+        public int? Kategorija { get; set; }
 
         [IgnoreDataMember]
         [MaxFileSizeValidator(8 * 1024 * 1024, ErrorMessage = "Maximum allowed file size is {0} bytes")]
