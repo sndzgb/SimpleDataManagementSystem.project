1f01f38c2826aaf8bf566ca787991e168579615b
sandro <noemail@lackingdomain.co>
2024-11-11 15:12:35 +0100

    added new page to redirect forbidden status code to

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Forbidden.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages
+{
+    // TODO rename to "ErrorPage403"
+    public class ForbiddenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
