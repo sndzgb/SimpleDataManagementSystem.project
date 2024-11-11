a00426808355bf41cf2346ad0278b65661dace93
sandro <noemail@lackingdomain.co>
2024-11-11 15:19:30 +0100

    updated menu (navbar) styling and added permissions check

--- SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Shared/_Menu.cshtml.cs
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Shared/_Menu.cshtml.cs
@@ -1,12 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Shared
 {
-    public class _MenuModel : PageModel
+    public class _MenuModel : PageModel // TODO BasePageModel
     {
-
-
         public void OnGet()
         {
         }
