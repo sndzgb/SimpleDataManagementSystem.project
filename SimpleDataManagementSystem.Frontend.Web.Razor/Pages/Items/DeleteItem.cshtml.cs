c39056a9f8a6854d25466b00c6544bcc156eeb46
sandro <noemail@lackingdomain.co>
2024-11-11 14:30:02 +0100

    updated delete item - it now inherits from base page model; implemented authorization based on defined policies; updated html to reflect those changes

--- SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Items/DeleteItem.cshtml.cs
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Items/DeleteItem.cshtml.cs
@@ -1,37 +1,64 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Base;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Services;
+using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read;
+using SimpleDataManagementSystem.Shared.Common.Constants;
 using System.Net;
 
 namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Items
 {
     [ValidateAntiForgeryToken]
-    [Authorize(Roles = "Admin,Employee")]
-    public class DeleteItemModel : PageModel
+    public class DeleteItemModel : BasePageModel<ItemViewModel>
     {
         private readonly IItemsService _itemsService;
+        private readonly IAuthorizationService _authorizationService;
 
 
-        public DeleteItemModel(IItemsService itemsService)
+        public DeleteItemModel(IItemsService itemsService, IAuthorizationService authorizationService)
         {
             _itemsService = itemsService;
+            _authorizationService = authorizationService;
         }
 
 
         [FromRoute]
         public string ItemId { get; set; }
 
-        
-        public void OnGet()
+
+        public override async void OnPageHandlerExecuting(PageHandlerExecutingContext context)
         {
+            int[] roles = new int[] { (int)Roles.Admin, (int)Roles.Employee };
+
+            AuthorizationResult authorizationResult = await _authorizationService.AuthorizeAsync(
+                HttpContext.User,
+                new { roles },
+                Policies.PolicyNames.UserIsInRole
+            );
+
+            if (!authorizationResult.Succeeded)
+            {
+                context.Result = Forbid();
+                return;
+            }
+
+            base.OnPageHandlerExecuting(context);
         }
 
-        public async Task<IActionResult> OnPostDeleteItem()
+        public async Task<IActionResult> OnGet()
+        {
+            Model = await _itemsService.GetItemByIdAsync(ItemId);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
         {
             await _itemsService.DeleteItemAsync(ItemId);
 
-            return new JsonResult(null) { StatusCode = (int)HttpStatusCode.OK };
+            return RedirectToPage("/Items/Items");
         }
     }
 }
