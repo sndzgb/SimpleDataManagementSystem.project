5815766912b8015d99b6a67a295a5e9858dd0b67
sandro <noemail@lackingdomain.co>
2024-11-11 14:20:19 +0100

    updated delete user - it now inherits from base page model; updated html

--- SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Users/DeleteUser.cshtml.cs
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Users/DeleteUser.cshtml.cs
@@ -1,37 +1,71 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Base;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Services;
+using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read;
+using SimpleDataManagementSystem.Shared.Common.Constants;
+using SimpleDataManagementSystem.Shared.Common.Policies;
 using System.Net;
 
 namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages
 {
     [ValidateAntiForgeryToken]
-    [Authorize(Roles = "Admin")]
-    public class DeleteUserModel : PageModel
+    public class DeleteUserModel : BasePageModel<UserViewModel>
     {
         private readonly IUsersService _usersService;
+        private readonly IAuthorizationService _authorizationService;
 
 
-        public DeleteUserModel(IUsersService usersService)
+        public DeleteUserModel(IUsersService usersService, IAuthorizationService authorizationService)
         {
             _usersService = usersService;
+            _authorizationService = authorizationService;
         }
 
 
         [FromRoute]
         public int UserId { get; set; }
 
-
-        public void OnGet()
+        
+        public override async void OnPageHandlerExecuting(PageHandlerExecutingContext context)
         {
+            int[] roles = new int[] { (int)Roles.Admin };
+
+            AuthorizationResult isInRole = await _authorizationService.AuthorizeAsync(
+                HttpContext.User,
+                new { roles },
+                Policies.PolicyNames.UserIsInRole
+            );
+
+            AuthorizationResult isResourceOwner = await _authorizationService.AuthorizeAsync(
+                HttpContext.User,
+                UserId,
+                Policies.PolicyNames.UserIsResourceOwner
+            );
+
+            if (!isInRole.Succeeded && !isResourceOwner.Succeeded)
+            {
+                context.Result = Forbid();
+                return;
+            }
+
+            base.OnPageHandlerExecuting(context);
         }
 
-        public async Task<IActionResult> OnPostDeleteUser()
+        public async Task<IActionResult> OnPost()
         {
             await _usersService.DeleteUserAsync(UserId);
 
-            return new JsonResult(null) { StatusCode = (int)HttpStatusCode.OK };
+            return RedirectToPage("/Users/Users");
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            Model = await _usersService.GetUserByIdAsync(UserId);
+
+            return Page();
         }
     }
 }
