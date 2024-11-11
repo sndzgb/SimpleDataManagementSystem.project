744c7f41315c83bde2b6ba47ad818ddfae79bfb6
sandro <noemail@lackingdomain.co>
2024-11-11 14:25:19 +0100

    updated delete retailer - it now inherits from base page model; implemented authorization based on defined policies; updated html

--- SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Retailers/DeleteRetailer.cshtml.cs
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Retailers/DeleteRetailer.cshtml.cs
@@ -1,37 +1,64 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Base;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Services;
+using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read;
+using SimpleDataManagementSystem.Shared.Common.Constants;
 using System.Net;
 
 namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Retailers
 {
     [ValidateAntiForgeryToken]
-    [Authorize(Roles = "Admin,Employee")]
-    public class DeleteRetailerModel : PageModel
+    public class DeleteRetailerModel : BasePageModel<RetailerViewModel>
     {
         private readonly IRetailersService _retailersService;
+        private readonly IAuthorizationService _authorizationService;
 
 
-        public DeleteRetailerModel(IRetailersService retailersService)
+        public DeleteRetailerModel(IRetailersService retailersService, IAuthorizationService authorizationService)
         {
             _retailersService = retailersService;
+            _authorizationService = authorizationService;
         }
 
 
         [FromRoute]
         public int RetailerId { get; set; }
 
-
-        public void OnGet()
+        
+        public override async void OnPageHandlerExecuting(PageHandlerExecutingContext context)
         {
+            int[] roles = new int[] { (int)Roles.Admin, (int)Roles.Employee };
+
+            AuthorizationResult authorizationResult = await _authorizationService.AuthorizeAsync(
+                HttpContext.User,
+                new { roles },
+                Policies.PolicyNames.UserIsInRole
+            );
+
+            if (!authorizationResult.Succeeded)
+            {
+                context.Result = Forbid();
+                return;
+            }
+
+            base.OnPageHandlerExecuting(context);
         }
 
-        public async Task<IActionResult> OnPostDeleteRetailer()
+
+        public async Task<IActionResult> OnGet()
+        {
+            Model = await _retailersService.GetRetailerByIdAsync(RetailerId);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
         {
             await _retailersService.DeleteRetailerAsync(RetailerId);
-
-            return new JsonResult(null) { StatusCode = (int)HttpStatusCode.OK };
+            return RedirectToPage("/Retailers/Retailers");
         }
     }
 }
