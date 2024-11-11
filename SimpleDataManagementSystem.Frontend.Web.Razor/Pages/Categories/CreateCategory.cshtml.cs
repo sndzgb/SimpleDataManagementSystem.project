c248b5ff16aac97ce5f91782b9de3192df2602f0
sandro <noemail@lackingdomain.co>
2024-11-11 14:34:12 +0100

    updated create category - it now inherits from base page model; implemented authorization based on defined policies; updated html to reflect those changes

--- SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Categories/CreateCategory.cshtml.cs
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/Categories/CreateCategory.cshtml.cs
@@ -1,52 +1,58 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Exceptions;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Base;
 using SimpleDataManagementSystem.Frontend.Web.Razor.Services;
 using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Write;
+using SimpleDataManagementSystem.Shared.Common.Constants;
 
 namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.Categories
 {
-    [Authorize(Roles = "Admin,Employee")]
-    public class CreateCategoryModel : PageModel
+    public class CreateCategoryModel : BasePageModel<NewCategoryViewModel>
     {
         private readonly ICategoriesService _categoriesService;
+        private readonly IAuthorizationService _authorizationService;
 
 
-        public CreateCategoryModel(ICategoriesService categoriesService)
+        public CreateCategoryModel(ICategoriesService categoriesService, IAuthorizationService authorizationService)
         {
             _categoriesService = categoriesService;
+            _authorizationService = authorizationService;
         }
 
 
-        [BindProperty]
-        public NewCategoryViewModel NewCategory { get; set; }
+        public override async void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+        {
+            int[] roles = new int[] { (int)Roles.Admin, (int)Roles.Employee };
 
-        public WebApiCallException Error { get; set; }
+            AuthorizationResult authorizationResult = await _authorizationService.AuthorizeAsync(
+                HttpContext.User,
+                new { roles },
+                Policies.PolicyNames.UserIsInRole
+            );
+
+            if (!authorizationResult.Succeeded)
+            {
+                context.Result = Forbid();
+                return;
+            }
+
+            base.OnPageHandlerExecuting(context);
+        }
 
 
         public async Task<IActionResult> OnGet()
         {
-            return null;
+            return Page();
         }
 
         public async Task<IActionResult> OnPost(NewCategoryViewModel newCategoryViewModel)
         {
-            try
-            {
-                if (!ModelState.IsValid)
-                {
-                    return await OnGet();
-                }
-                var newUserId = await _categoriesService.AddNewCategoryAsync(newCategoryViewModel);
+            var newCategory = await _categoriesService.AddNewCategoryAsync(newCategoryViewModel);
 
-                return RedirectToPage("/Categories/Categories");
-            }
-            catch (WebApiCallException wace)
-            {
-                Error = wace; // set error on model
-                return await OnGet();
-            }
+            return RedirectToPage("/Categories/Categories");
         }
     }
 }
