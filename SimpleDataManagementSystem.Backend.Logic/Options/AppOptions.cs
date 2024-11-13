294c513cb8e47df20aaff1ce673d97d7dd820fc4
sandro <noemail@lackingdomain.co>
2024-11-13 01:44:33 +0100

    added new options classes for app and email client

---
+++ SimpleDataManagementSystem.Backend.Logic/Options/AppOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataManagementSystem.Backend.Logic.Options
+{
+    public sealed class AppOptions
+    {
+        /// <summary>
+        /// Section name.
+        /// </summary>
+        public const string AppOptionsSectionName = "AppOptions";
+
+        public string AdminEmail { get; set; }
+    }
+}
---
+++ SimpleDataManagementSystem.Backend.Logic/Options/EmailClientOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataManagementSystem.Backend.Logic.Options
+{
+    public class EmailClientOptions
+    {
+        /// <summary>
+        /// Section name.
+        /// </summary>
+        public const string EmailOptionsSectionName = "EmailClientOptions";
+
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+        public string NoReplyEmail { get; set; }
+        public NetworkCredentials? Credentials { get; set; }
+
+        public class NetworkCredentials
+        {
+            public string? Username { get; set; }
+            public string? Password { get; set; }
+        }
+    }
+}
