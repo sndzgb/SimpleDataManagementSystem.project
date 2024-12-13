61fa8858c76db17e16cccd848a9b5357c0887d15
sandro <noemail@lackingdomain.co>
2024-12-13 04:28:44 +0100

    updated razor pages httpResponseMessage handling logic

--- SimpleDataManagementSystem.Frontend.Web.Razor/Extensions/HttpResponseMessageExtensions.cs
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Extensions/HttpResponseMessageExtensions.cs
@@ -1,5 +1,6 @@
-﻿using SimpleDataManagementSystem.Frontend.Web.Razor.Exceptions;
-using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels.Read;
+﻿using Microsoft.IdentityModel.Tokens;
+using SimpleDataManagementSystem.Frontend.Web.Razor.Exceptions;
+using SimpleDataManagementSystem.Frontend.Web.Razor.ViewModels;
 using System.Text.Json;
 
 namespace SimpleDataManagementSystem.Frontend.Web.Razor.Extensions
@@ -28,11 +29,11 @@ namespace SimpleDataManagementSystem.Frontend.Web.Razor.Extensions
         //    throw new WebApiCallException(message);
         //}
 
-        public static async Task<T?> HandleResponseAsync<T>(this HttpResponseMessage httpResponseMessage)
+        public static async Task<T?> HandleResponseAsync<T>(this HttpResponseMessage httpResponseMessage, CancellationToken cancellationToken)
         {
             if (httpResponseMessage.IsSuccessStatusCode)
             {
-                var json = await httpResponseMessage.Content.ReadAsStringAsync();
+                var json = await httpResponseMessage.Content.ReadAsStringAsync(cancellationToken);
 
                 if (string.IsNullOrEmpty(json))
                 {
@@ -43,20 +44,34 @@ namespace SimpleDataManagementSystem.Frontend.Web.Razor.Extensions
 
                 return await Task.FromResult(model);
             }
-
-            if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            else
             {
-                throw new UnauthorizedException("You must be logged in to view this resource");
-            }
+                //if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                //{
+                //    throw new UnauthorizedException("You must be logged in to view this resource");
+                //}
 
-            if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.Forbidden)
-            {
-                throw new ForbiddenException("You are not allowed to access this resource");
-            }
+                //if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.Forbidden)
+                //{
+                //    throw new ForbiddenException("You are not allowed to access this resource");
+                //}
 
-            using var contentStream = await httpResponseMessage.Content.ReadAsStreamAsync();
-            var message = await JsonSerializer.DeserializeAsync<ErrorViewModel>(contentStream);
-            throw new WebApiCallException(message);
+                var jsonResponse = await httpResponseMessage.Content.ReadAsStringAsync(cancellationToken);
+
+                var responseMessage = new ErrorViewModel((int)httpResponseMessage.StatusCode, httpResponseMessage.ReasonPhrase, null);
+
+                if (!jsonResponse.IsNullOrEmpty())
+                {
+                    responseMessage = JsonSerializer.Deserialize<ErrorViewModel>(jsonResponse);
+                }
+
+                throw new WebApiCallException(responseMessage);
+
+
+                using var contentStream = await httpResponseMessage.Content.ReadAsStreamAsync(cancellationToken);
+                var message = await JsonSerializer.DeserializeAsync<ErrorViewModel>(contentStream);
+                throw new WebApiCallException(message);
+            }
         } 
     }
 }
