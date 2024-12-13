3b7354e319a34cd17fd899b0ed9116634f3f5278
sandro <noemail@lackingdomain.co>
2024-12-13 04:09:21 +0100

    added base web api controller

---
+++ SimpleDataManagementSystem.Backend.WebAPI/Controllers/Base/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleDataManagementSystem.Backend.WebAPI.Controllers.Base
+{
+    [Controller]
+    public abstract class BaseController : ControllerBase
+    {
+        protected BaseController()
+        {
+        }
+
+        protected int GetUserId()
+        {
+            return Convert.ToInt32(HttpContext.User.Identity?.Name);
+        }
+    }
+}
