ea8e28bdd01ed5db72fcdab2a9b4e350f6fce929
sandro <noemail@lackingdomain.co>
2024-12-13 04:25:08 +0100

    updated removed error web api model

---
+++ SimpleDataManagementSystem.Backend.WebAPI/WebApiModels/ErrorWebApiModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SimpleDataManagementSystem.Backend.WebAPI.WebApiModels
+{
+    public class ErrorWebApiModel
+    {
+        public ErrorWebApiModel(int statusCode, string? message, List<string>? errors = null)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Errors = errors;
+        }
+
+
+        [JsonPropertyName("statusCode")]
+        public int StatusCode { get; private set; }
+
+        [JsonPropertyName("message")]
+        public string? Message { get; private set; }
+
+        [JsonPropertyName("errors")]
+        public List<string> Errors { get; private set; }
+    }
+}
