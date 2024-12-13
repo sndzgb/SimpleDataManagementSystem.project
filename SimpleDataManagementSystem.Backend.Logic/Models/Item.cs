3e944a7bc0b612529c98cd5a78e0b40c8523c27f
sandro <noemail@lackingdomain.co>
2024-12-13 05:44:03 +0100

    removed item core model duplicate properties

--- SimpleDataManagementSystem.Backend.Logic/Models/Item.cs
+++ SimpleDataManagementSystem.Backend.Logic/Models/Item.cs
@@ -17,9 +17,6 @@ namespace SimpleDataManagementSystem.Backend.Logic.Models
         public string? Nazivretailera { get; set; }
         public string? URLdoslike { get; set; }
 
-        public string? URLdoslike { get; set; }
-        public decimal Cijena { get; set; }
-
         public List<MonitoredItem>? Monitored { get; set; }
         public Category? Category { get; set; }
         public Retailer Retailer { get; set; }
