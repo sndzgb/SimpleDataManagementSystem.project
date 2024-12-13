96f50a584bc1bc7754f5a77621f3e47c470b8b36
sandro <noemail@lackingdomain.co>
2024-12-13 04:29:46 +0100

    added new razor pages filter, ajaxOnly, which limits requests to be only of xml (ajax) request type

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Filters/AjaxOnlyPageFilterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Filters
+{
+    public class AjaxOnlyPageFilterAttribute : Attribute, IAsyncPageFilter
+    {
+        public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+        {
+            if (context.HttpContext.Request.Headers["X-Requested-With"] != "XMLHttpRequest")
+            {
+                context.Result = new ObjectResult(null)
+                {
+                    StatusCode = StatusCodes.Status404NotFound
+                };
+            } 
+            else
+            {
+                await next();
+            }
+        }
+
+        public Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
