2f431101f0e9ba564c4f6dde09ebf281b388d118
sandro <noemail@lackingdomain.co>
2024-12-13 04:14:49 +0100

    added sqliteOptions, for tracking signalR user connections

---
+++ SimpleDataManagementSystem.Backend.WebAPI/Options/SqliteOptions.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace SimpleDataManagementSystem.Backend.WebAPI.Options
+{
+    public class SqliteOptions
+    {
+        public const string SqliteOptionsSectionName = "SqliteOptions";
+
+        public string HubUserStateManagementDbName { get; set; }
+
+        public string GetConnectionString()
+        {
+            var path = Path.Combine(
+                    Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)!,
+                    HubUserStateManagementDbName
+                );
+
+            var db = string.Concat("Data Source=", path);
+
+            return db;
+        }
+    }
+}
