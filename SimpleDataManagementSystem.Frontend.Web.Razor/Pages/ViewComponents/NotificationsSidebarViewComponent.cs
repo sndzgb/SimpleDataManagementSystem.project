24cab9b1e86312a5990e436ac47f7ab1affc99c2
sandro <noemail@lackingdomain.co>
2024-12-16 21:08:55 +0100

    moved notifications to its own component

---
+++ SimpleDataManagementSystem.Frontend.Web.Razor/Pages/ViewComponents/NotificationsSidebarViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SimpleDataManagementSystem.Frontend.Web.Razor.Pages.ViewComponents
+{
+    public class NotificationsSidebarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
